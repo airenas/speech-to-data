baf14f708c780ae984251093856db10e12dac54d
Airenas Vaiciunas <airenass@gmail.com>
2023-12-21 00:05:07 +0200

    Fix lint errors

--- src/js/gui/lines.js
+++ src/js/gui/lines.js
@@ -1,60 +1,55 @@
 export default class Lines {
-    constructor(div) {
-        this.lines = []
-        this.div = div
+  constructor (div) {
+    this.lines = []
+    this.div = div
+  }
+
+  add (line) {
+    this.lines.push(line)
+    this.div.appendChild(line.div)
+  }
+
+  hasText () {
+    for (let i = 0; i < this.lines.length; i++) {
+      const child = this.lines[i]
+      const text = child.text()
+      if (text.length > 0) {
+        return true
+      }
     }
+    return false
+  }
 
-    add(line) {
-        this.lines.push(line)
-        this.div.appendChild(line.div)
+  editable (value) {
+    for (let i = 0; i < this.lines.length; i++) {
+      this.lines[i].editable(value)
     }
+  }
 
-    hasText() {
-        const children = this.lines
+  clear () {
+    this.lines = []
+    while (this.div.childElementCount > 0) {
+      this.div.removeChild(this.div.lastElementChild)
+    }
+  }
 
-        for (var i = 0; i < this.lines.length; i++) {
-            const child = this.lines[i];
-            const text = child.text();
-            if (text.length > 0) {
-                return true
-            }
+  selectAll () {
+    for (let i = 0; i < this.lines.length; i++) {
+      this.lines[i].markChecked()
+    }
+  }
+
+  getSelectedText () {
+    let res = ''
+    for (let i = 0; i < this.lines.length; i++) {
+      const line = this.lines[i]
+      if (line.isChecked()) {
+        if (res.length > 0) {
+          res += '\n'
         }
-        return false
+        res += line.text()
+      }
     }
-
-    editable(value) {
-        const children = this.lines
-        for (var i = 0; i < this.lines.length; i++) {
-            this.lines[i].editable(value);
-        }
-    }
-
-    clear() {
-        this.lines = []
-        while (this.div.childElementCount > 0) {
-            this.div.removeChild(this.div.lastElementChild);
-        }
-    }
-
-    selectAll() {
-        const children = this.lines
-        for (var i = 0; i < this.lines.length; i++) {
-            this.lines[i].markChecked();
-        }
-    }
-
-    getSelectedText() {
-        let res = ''
-        const children = this.lines
-        for (var i = 0; i < this.lines.length; i++) {
-            const line = this.lines[i];
-            if (line.isChecked()) {
-                if (res.length > 0) {
-                    res += "\n"
-                }
-                res += line.text();
-            }
-        }
-        return res
-    }
+    return res
+  }
 }
