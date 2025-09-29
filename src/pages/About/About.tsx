5a9deb73ac69a2b83af19770adf5a8159c996afa
Airenas Vaiciunas <airenass@gmail.com>
2025-09-29 18:00:23 +0300

    feat: implement demo tour functionality in About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,44 @@
 import Typography from '@mui/material/Typography';
 
+import { useAppContext } from '@/app-context/AppContext';
 import Meta from '@/components/Meta';
 import { FullSizeCenteredFlexBox } from '@/components/styled';
+import configService from '@/services/configService';
 import { VERSION } from '@/version';
+import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 function About() {
+  const { user } = useAppContext();
+  const [isDemoEnabled, setIsDemoEnabled] = useState(false);
+
+  useEffect(() => {
+    if (user && !user.skipTour) {
+      setIsDemoEnabled(true);
+    } else {
+      setIsDemoEnabled(false);
+    }
+  }, [user]);
+
+  function turnOnDemoTour() {
+    if (!user) {
+      return;
+    }
+    (
+      async () => {
+        try {
+          await configService.save({ skipTour: false });
+          setIsDemoEnabled(true);
+          if (user) {
+            user.skipTour = false;
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    )();
+  }
+
   return (
     <>
       <Meta title="apie" />
@@ -13,6 +47,12 @@ function About() {
           <Typography variant="h2">DiPolis Audio Transkribatorius</Typography>
           <Typography variant="body1">Sukurta: VDU</Typography>
           <Typography variant="body1">Versija: {VERSION}</Typography>
+
+          <Button variant="contained" color="primary" disabled={isDemoEnabled} onClick={turnOnDemoTour}
+            style={{ display: !user ? 'none' : 'block' }}
+          >
+            Įjungti demo turą
+          </Button>
         </div>
       </FullSizeCenteredFlexBox>
     </>
