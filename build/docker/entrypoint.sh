37787ea58e407827adb995f5be5cb4738bdc410e
Airenas Vaiciunas <airenass@gmail.com>
2025-09-30 14:12:05 +0300

    fix: docker build

--- build/docker/entrypoint.sh
+++ build/docker/entrypoint.sh
@@ -9,6 +9,10 @@ echo "Set SERVER_URL to '$SERVER_URL'"
 find /usr/share/nginx/html -type f -exec sed -i "s|__SERVER_URL__|$SERVER_URL|g" {} +
 echo "Set SERVER_URL url to '$SERVER_URL' done"
 
+echo "Set DATA_SERVER_URL to '$DATA_SERVER_URL'"
+find /usr/share/nginx/html -type f -exec sed -i "s|__DATA_SERVER_URL__|$DATA_SERVER_URL|g" {} +
+echo "Set DATA_SERVER_URL url to '$DATA_SERVER_URL' done"
+
 echo "Set AUTH_URL to '$AUTH_URL'"
 find /usr/share/nginx/html -type f -exec sed -i "s|__AUTH_URL__|$AUTH_URL|g" {} +
 echo "Set AUTH_URL url to '$AUTH_URL' done"
